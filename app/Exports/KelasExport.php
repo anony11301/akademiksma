5f3c1974b801a0f52bc5d84da8be15c0d37bd7f7
anony11301 <fredrikoswald54@gmail.com>
2023-10-29 15:33:37 +0700

    .

--- app/Exports/KelasExport.php
+++ app/Exports/KelasExport.php
@@ -3,11 +3,9 @@
 namespace App\Exports;
 
 use App\Models\Kelas;
-use Maatwebsite\Excel\Concerns\FromCollection;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
-use Maatwebsite\Excel\Concerns\WithHeadings;
 
 class KelasExport implements FromView, ShouldAutoSize
 {
--- resources/views/export/kelas.blade.php
+++ resources/views/export/kelas.blade.php
@@ -1,16 +1,16 @@
 <table>
     <thead>
         <tr>
-            <th style="font-family: calibri; font-size: 12px; text-align: center; border: 1px solid black; background-color: yellow;">No</th>
-            <th style="font-family: calibri; font-size: 12px; text-align: center; border: 1px solid black; background-color: yellow;">Nama Kelas</th>
+            <th style="font-family: calibri; font-size: 12px; border: 1px solid black; background-color: yellow;">No</th>
+            <th style="font-family: calibri; font-size: 12px; border: 1px solid black; background-color: yellow;">Nama Kelas</th>
         </tr>
     </thead>
     <tbody>
     @php $no = 1 @endphp
-    @foreach($kelas as $kelas)
+    @foreach ($kelas as $kelas)
         <tr>
-            <td style="border: 1px solid black;">{{ $no++ }}</td>
-            <td style="border: 1px solid black;">{{ $kelas->nama_kelas }}</td>
+            <td style="font-family: calibri; border: 1px solid black;">{{ $no++ }}</td>
+            <td style="font-family: calibri; border: 1px solid black;">{{ $kelas->nama_kelas }}</td>
         </tr>
     @endforeach
     </tbody>
