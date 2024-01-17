9a864f6d27378b10a314948072f584d82ccfb011
DikaPutra1205 <duia.putra12@gmail.com>
2024-01-17 14:18:13 +0700

    pembina

--- database/seeders/Management.php
+++ database/seeders/Management.php
@@ -15,35 +15,36 @@ class Management extends Seeder
     {
         $users = [
             [
-                'nama' => 'Management',
-                'email' => 'management@example.com',
-                'password' => bcrypt('management123'),
-                'id_level' => '1'
-            ],
-            [
-                'nama' => 'Della Novianti, S.Pd',
-                'email' => 'della@sma.prestasiprima.sch.id',
-                'password' => bcrypt('stafftas'),
-                'id_level' => '1'
-            ],
-            [
-                'nama' => 'Aulia Rahmayani Taofik, S.Pd',
-                'email' => 'auliar@sma.prestasiprima.sch.id',
-                'password' => bcrypt('stafftas'),
-                'id_level' => '1'
-            ],
-            [
-                'nama' => 'Ayuk Kristanti, SE',
-                'email' => 'ayuk@sma.prestasiprima.sch.id',
-                'password' => bcrypt('stafftas'),
-                'id_level' => '1'
-            ],
-            [
-                'nama' => 'Heni Dwi Riyanti, S.Pd',
-                'email' => 'heni@sma.prestasiprima.sch.id',
+                'nama' => 'Pembina Siswa',
+                'email' => 'pembina@gmail.com',
                 'password' => bcrypt('stafftas'),
                 'id_level' => '1'
             ]
+            // ,
+            // [
+            //     'nama' => 'Della Novianti, S.Pd',
+            //     'email' => 'della@sma.prestasiprima.sch.id',
+            //     'password' => bcrypt('stafftas'),
+            //     'id_level' => '1'
+            // ],
+            // [
+            //     'nama' => 'Aulia Rahmayani Taofik, S.Pd',
+            //     'email' => 'auliar@sma.prestasiprima.sch.id',
+            //     'password' => bcrypt('stafftas'),
+            //     'id_level' => '1'
+            // ],
+            // [
+            //     'nama' => 'Ayuk Kristanti, SE',
+            //     'email' => 'ayuk@sma.prestasiprima.sch.id',
+            //     'password' => bcrypt('stafftas'),
+            //     'id_level' => '1'
+            // ],
+            // [
+            //     'nama' => 'Heni Dwi Riyanti, S.Pd',
+            //     'email' => 'heni@sma.prestasiprima.sch.id',
+            //     'password' => bcrypt('stafftas'),
+            //     'id_level' => '1'
+            // ]
         ];
 
         foreach ($users as $key => $value) {
